04227f84eee24a6fdbb22bc6458229e29ee9e87a
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-12-08 22:45:48 +0500

    Initial Commit

---
+++ QRHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRHub
+//
+//  Created by Muhammad Asad Chattha on 08/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRHub/QRHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRHubApp.swift
+//  QRHub
+//
+//  Created by Muhammad Asad Chattha on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
