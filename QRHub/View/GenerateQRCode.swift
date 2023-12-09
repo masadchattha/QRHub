d2a7627b987597c36391c8b713221df99c63bfdd
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-12-09 09:53:41 +0500

    feat: set up project for generating QR codes

---
+++ QRHub/View/GenerateQRCode.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateQRCode.swift
+//  QRHub
+//
+//  Created by Muhammad Asad Chattha on 08/12/2023.
+//
+
+import SwiftUI
+
+struct GenerateQRCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenerateQRCode()
+}
